1a1b446935454155bdd2aeeb40a957fadfc49618
cadovnik <hotchenkov@list.ru>
2016-02-12 14:45:27 +0300

    some fixes

--- main.cpp
+++ main.cpp
@@ -17,11 +17,7 @@ void fileread(const char* filepath,vector<string>* instr){
 
 string passGen(char* s){
 	
-//	char s[]="qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM1234567890!@#";
 	string pass;
-//	vector<string> p;
-//	boost::split(p,*s,boost::is_any_of("|"));
-//	cout<<p.size();
 	for(int i=0;i<12;i++){
 		int gen=rand()%65;
 		pass+=s[gen];
@@ -42,7 +38,7 @@ void filewrite(const char* filepath,vector<string>* out){
 int main(int argc, char* argv[]) {
 	
 
-//	char s[]="q|Q|w|W|e|E|r|R|t|T|y|Y|u|U|i|I|o|O|p|P|a|A|s|S|d|D|f|F|g|G|h|H|j|J|k|K|l|L|z|Z|x|X|c|C|v|V|b|B|n|N|m|M|1|2|3|4|5|6|7|8|9|0|!|@|#";
+
 	char s[]="qQwWeErRtTyYuUiIoOpPaAsSdDfFgGhHjJkKlLzZxXcCvVbBnNmM1234567890!@#";
 		srand(time(NULL));
 
