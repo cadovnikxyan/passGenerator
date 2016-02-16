dbbd0dbc5e353efa90c7b0d8cc6cbb6646708215
cadovnik <hotchenkov@list.ru>
2016-02-16 09:42:55 +0300

    add flag description

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ void filewrite(const char* filepath,vector<string>* out){
 		}
 }
 
-void generator(const char* filepath){
+void generator(const char* filepath,const char* fileout){
 	
 			vector<string> instr;
 			vector<string> outstr;
@@ -44,25 +44,54 @@ void generator(const char* filepath){
 			for(size_t i=0;i<instr.size();i++){
 				if(instr.at(i)!=""){
 				                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
-				outstr.push_back(instr.at(i)+passGen());
+				outstr.push_back(instr.at(i)+" "+passGen());
 				}
 			}
-			filewrite("out.txt",&outstr);
+			filewrite(fileout,&outstr);
 			
+}
+void help(){
+				cout<<"this programm for generated passwords"<<endl
+				<<"flag description:"<<endl
+				<<"flag -I enter input file text data  "<<endl
+				<<"file structure is "<<endl
+				<<"Full name - department -"<<endl
+				<<"flag -O if you want to customize output file name"<<endl
+				<<"flag -S generated single password"<<endl;
 }
 
 int main(int argc, char* argv[]) {
-	
-
-
-		
+			
 		srand(time(NULL));
 
 		if(argc>1){
-			
-				generator(argv[1]);	
+			if(argc>3){
+				if(strcmp(argv[3],"-O")==0){
+					generator(argv[2],argv[4]);	
+					cout<<"passwords generated!"<<endl;	
+				}
+				
+			}else if(strcmp(argv[1],"-I")==0){					
+				generator(argv[2],"out.txt");	
+				cout<<"passwords generated!"<<endl;	
+										
+			}else if(strcmp(argv[1],"-S")==0){
+				cout<<"Single password generated!"<<endl;
+				cout<<passGen()<<endl;
+			}else if(strcmp(argv[1],"-h")==0){
+				help();
+			}else{
+				help();
+			}
+		}else{
+			help();			
 		}
-
-
+		
+		
 	return 0;
 }
+
+
+
+
+
